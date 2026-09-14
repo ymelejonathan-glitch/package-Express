1a4ab6acef5813f0f187fca1e3331bd9a15e02f4
ymelejonathan-glitch <ymelejonathan@gmail.com>
2026-09-14 18:52:44 -0400

    dotnet run

--- MathClass.cs
+++ MathClass.cs
@@ -2,14 +2,16 @@ using System;
 
 namespace PackageExpress
 {
-    // This class contains the method required for the assignment.
     public class MathClass
     {
-    }// This void method takes two integer parameters.
-public void PerformMathOperation(int number1, int number2)
-{
-    // This performs a math operation on the first integer.
-    int result = number1 + 10;
+        // This method takes two integers.
+        public void PerformMathOperation(int number1, int number2)
+        {
+            // Perform a math operation on the first integer.
+            int result = number1 + 10;
 
-    // This displays the second integer on the screen.
-    Console.WriteLine(number2)
+            // Display the second integer.
+            Console.WriteLine(number2);
+        }
+    }
+}
